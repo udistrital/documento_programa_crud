2e76afd43b007c582ad9e2dcf27d64058d428c97
David millan <damillano@correo.udistrital.edu.co>
2019-05-27 10:51:24 -0500

    nueva prueba

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ if [ -n "${PARAMETER_STORE:-}" ]; then
   export ENTE_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ente_crud/db/username --output text --query Parameter.Value)"
   export ENTE_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ente_crud/db/password --output text --query Parameter.Value)"
 fi
-
+exec DOCUMENTO_PROGRAMA_CRUD__PGDB=$DOCUMENTO_PROGRAMA_CRUD__PGDB DOCUMENTO_PROGRAMA_CRUD__PGPASS=$DOCUMENTO_PROGRAMA_CRUD__PGPASS DOCUMENTO_PROGRAMA_CRUD__PGURLS=$DOCUMENTO_PROGRAMA_CRUD__PGURLS DOCUMENTO_PROGRAMA_CRUD__PGUSER=$DOCUMENTO_PROGRAMA_CRUD__PGURLS DOCUMENTO_PROGRAMA_CRUD__SCHEMA=$DOCUMENTO_PROGRAMA_CRUD__SCHEMA DOCUMENTO_PROGRAMA_HTTP_PORT=$DOCUMENTO_PROGRAMA_HTTP_PORT ./main orm syncdb -db="default" -force=true -v=true
 exec ./main "$@" 
 
